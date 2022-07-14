dadf8b83e73e91446539d800dbf35ce6fd38fa7f
You Niform <youniform@yahoo.com>
2022-07-14 18:41:13 -0400

    Was able to get the AJAX request to return successfully, will need to figure out why it isn't rendering the results section on the same page though

---
+++ CustaccountTab/Controller/Result/Result.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CrimsonAgility\CustaccountTab\Controller\Result;
+
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Controller\Result\JsonFactory;
+
+class Result extends \Magento\Framework\App\Action\Action
+{
+
+     /**
+     * @var Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    protected $resultJsonFactory; 
+
+    /**
+     * @param Context     $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory,
+        JsonFactory $resultJsonFactory
+        )
+    {
+
+        $this->resultPageFactory = $resultPageFactory;
+        $this->resultJsonFactory = $resultJsonFactory; 
+        return parent::__construct($context);
+    }
+
+
+    public function execute()
+    {
+        $lowprice = $this->getRequest()->getParam('lowprice');
+        $highprice = $this->getRequest()->getParam('highprice');
+        $sort = $this->getRequest()->getParam('sortorder');
+
+        $result = $this->resultJsonFactory->create();
+        $resultPage = $this->resultPageFactory->create();
+
+        $block = $resultPage->getLayout()
+                ->createBlock('CrimsonAgility\CustaccountTab\Block\Index')
+                ->setTemplate('CrimsonAgility_CustaccountTab::result.phtml')
+                ->setData('lowprice',$lowprice)
+                ->setData('highprice',$highprice)
+                ->toHtml();
+        
+
+        $result->setData(['output' => $block]);
+        return $result; 
+    } 
+}
